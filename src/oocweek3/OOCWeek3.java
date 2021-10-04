6ba1d208d321ee5d4b9ebdd9bdbdb978c46dc3f7
Felipe <72029039+renanz21@users.noreply.github.com>
2021-10-04 13:37:37 +0100

    T1

--- src/oocweek3/OOCWeek3.java
+++ src/oocweek3/OOCWeek3.java
@@ -16,6 +16,9 @@ public class OOCWeek3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("eee");
+        System.out.println("eee");
+        System.out.println("eee");
     }
     
 }
